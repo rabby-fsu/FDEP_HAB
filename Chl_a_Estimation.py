0864be4e2adfe829e92b5d85dd9a6bc056f1f8af
rabby-fsu <147728383+rabby-fsu@users.noreply.github.com>
2024-02-22 21:08:45 -0500

    Update Chl_a_Estimation.py

--- Chl_a_Estimation.py
+++ Chl_a_Estimation.py
@@ -48,6 +48,8 @@ elif selected_page == 'Apalachicola Bay-Estuary':
     # Use combined_training_data for the following model
     X_train = combined_training_data[selected_features]
     y_train = combined_training_data['Chlorophyll-a (ug/L)']
+    X_test = combined_testing_data[selected_features]
+    y_test = combined_testing_data['Chlorophyll-a (ug/L)']
     
     # Initialize and fit the XGBoost Regressor
     xgb_regressor = XGBRegressor(n_estimators=334, max_depth=4, learning_rate=0.07818940902700418, random_state=42)
@@ -55,17 +57,24 @@ elif selected_page == 'Apalachicola Bay-Estuary':
     
     # Predictions
     y_train_pred = xgb_regressor.predict(X_train)
+    y_test_pred = xgb_regressor.predict(X_test)
     
     # Evaluation metrics
     train_r2 = r2_score(y_train, y_train_pred)
     train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
-
+    
+    test_r2 = r2_score(y_test, y_test_pred)
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
 
     # Display R2 and RMSE for training data
     st.write("## Training Data Metrics")
     st.write(f"Training R^2 Score: {train_r2}")
     st.write(f"Training RMSE: {train_rmse}")
-
+    # Display R2 and RMSE for testing data
+    st.write("## Test Data Metrics")
+    st.write(f"Test R^2 Score: {train_r2}")
+    st.write(f"Test RMSE: {train_rmse}")
+    
     # Initialize lists to store R2 and RMSE for each station's test data
     test_r2_scores = []
     test_rmse_scores = []
