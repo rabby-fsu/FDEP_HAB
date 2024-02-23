8c80444d79058621a066ae1993def7dff8da88c8
rabby-fsu <147728383+rabby-fsu@users.noreply.github.com>
2024-02-22 21:43:16 -0500

    Update Chl_a_Estimation.py

--- Chl_a_Estimation.py
+++ Chl_a_Estimation.py
@@ -7,6 +7,35 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pydeck as pdk
 
+# Load data
+df = pd.read_csv('DataFile_ML_All.csv')
+
+# Define selected features
+selected_features = ['Salinity(ppt)', 'Turbidity(NTU)', 'DO(mg/l)', 'pH', 'ATemp_max',
+                     'ATemp_max_1dlag', 'ATemp_max_2dlag', 'ATemp_max_3dlag',
+                     'ATemp_max_4dlag', 'ATemp_max_5dlag', 'ATemp_max_6dlag',
+                     'ATemp_max_7dlag']
+
+X = df[selected_features]
+y = df['Chlorophyll-a (ug/L)']
+
+# Perform train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+# Initialize and fit the XGBoost Regressor
+xgb_regressor = XGBRegressor(n_estimators=334, max_depth=4, learning_rate=0.07818940902700418, random_state=42)
+xgb_regressor.fit(X_train, y_train)
+    
+# Predictions
+y_train_pred = xgb_regressor.predict(X_train)
+y_test_pred = xgb_regressor.predict(X_test)
+    
+# Evaluation metrics
+train_r2 = r2_score(y_train, y_train_pred)
+train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
+    
+test_r2 = r2_score(y_test, y_test_pred)
+test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
 
 # Introduction Page
 st.sidebar.title('Pages')
@@ -17,39 +46,10 @@ if selected_page == 'Introduction':
     st.write('This is an application to evaluate the Apalachicola Bay Model.')
 
 elif selected_page == 'Apalachicola Bay-Estuary':
-    # Load data
-    df = pd.read_csv('DataFile_ML_All.csv')
-
-    # Define selected features
-    selected_features = ['Salinity(ppt)', 'Turbidity(NTU)', 'DO(mg/l)', 'pH', 'ATemp_max',
-                     'ATemp_max_1dlag', 'ATemp_max_2dlag', 'ATemp_max_3dlag',
-                     'ATemp_max_4dlag', 'ATemp_max_5dlag', 'ATemp_max_6dlag',
-                     'ATemp_max_7dlag']
     
     st.title('Gauged Stations')
     st.map(df,latitude='Latitude',longitude='Longitude',use_container_width=True)
 
-
-    X = df[selected_features]
-    y = df['Chlorophyll-a (ug/L)']
-
-    # Perform train-test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # Initialize and fit the XGBoost Regressor
-    xgb_regressor = XGBRegressor(n_estimators=334, max_depth=4, learning_rate=0.07818940902700418, random_state=42)
-    xgb_regressor.fit(X_train, y_train)
-    
-    # Predictions
-    y_train_pred = xgb_regressor.predict(X_train)
-    y_test_pred = xgb_regressor.predict(X_test)
-    
-    # Evaluation metrics
-    train_r2 = r2_score(y_train, y_train_pred)
-    train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
-    
-    test_r2 = r2_score(y_test, y_test_pred)
-    test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
 
     # Display R2 and RMSE for training data
     st.write("## Training Data Metrics")
